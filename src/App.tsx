86b2c7b764f54dde84f029413f028c68b26cabf0
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-07-14 00:15:10 -0300

    Criando o componente Header

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './routes/Home';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function Header() {
+
+    return(
+        <header>
+            <div className="container text-header">
+                <p>Github API</p>
+            </div>
+        </header>
+    );
+}
